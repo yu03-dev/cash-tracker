170dbf6b1a5d23220b08610e410fc8953ff7b5c8
yuto <yuto.0313king@icloud.com>
2023-08-08 20:03:28 +0900

    💄 フォームのスタイル変更

--- src/app/components/dashboard/RecordColumn/Amount.tsx
+++ src/app/components/dashboard/RecordColumn/Amount.tsx
@@ -15,7 +15,12 @@ export const Amount = ({
   return (
     <div>
       {isEditting ? (
-        <input type="text" onChange={handleEditAmount} value={editAmount} />
+        <input
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          type="text"
+          onChange={handleEditAmount}
+          value={editAmount}
+        />
       ) : (
         <Typography variant="small" color="blue-gray" className="font-normal">
           {amount + " 円"}
--- src/app/components/dashboard/RecordColumn/Purpose.tsx
+++ src/app/components/dashboard/RecordColumn/Purpose.tsx
@@ -15,7 +15,12 @@ export const Purpose = ({
   return (
     <div>
       {isEditting ? (
-        <input type="text" onChange={handleEditPurpose} value={editPurpose} />
+        <input
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          type="text"
+          onChange={handleEditPurpose}
+          value={editPurpose}
+        />
       ) : (
         <Typography variant="small" color="blue-gray" className="font-normal">
           {purpose}
