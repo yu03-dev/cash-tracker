3b288a18a422274979f80a6eb785cca342a36acb
yuto <yuto.0313king@icloud.com>
2023-08-12 03:39:22 +0900

    ♻️ firebase sdkの設定ファイルを変更

--- src/app/firebase/client.ts
+++ src/app/firebase/client.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,5 +13,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
