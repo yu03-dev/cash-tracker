888b22b196ae164555618f26c23b94ab668a0346
yuto <yuto.0313king@icloud.com>
2023-08-12 16:10:53 +0900

    ♻️ idTokenを用いた認証

--- src/app/api/records/route.ts
+++ src/app/api/records/route.ts
@@ -3,14 +3,16 @@ import { db, auth } from "@/app/firebase/admin";
 import { PostType } from "@/app/types";
 import { FieldValue } from "firebase-admin/firestore";
 
-export const GET = async () => {
-  // const idToken = req.headers.get("authorization");
-  // // idTokenが存在しないという旨のmessageをjsonで返すようにする？
-  // if (!idToken) return console.log("認証情報がありません");
+export const GET = async (request: NextRequest) => {
+  const idToken = request.headers.get("authorization");
+  if (!idToken)
+    return NextResponse.json(
+      { error: "認証情報がありません" },
+      { status: 401 }
+    );
   try {
-    // const decodedToken = await auth.verifyIdToken(idToken);
-    // const uid = decodedToken.uid;
-    const uid = "NhESkFqWjXaolAVkzyfKlgGZZFV2";
+    const decodedToken = await auth.verifyIdToken(idToken);
+    const uid = decodedToken.uid;
     const userRecordRef = db.collection("users").doc(uid).collection("records");
     const snapshot = await userRecordRef.orderBy("createdAt", "desc").get();
     const records = snapshot.docs.map((doc) => ({
@@ -19,22 +21,33 @@ export const GET = async () => {
     }));
     return NextResponse.json(records);
   } catch (error) {
-    console.error(error);
+    return NextResponse.json(error);
   }
 };
 
-export const POST = async (req: NextRequest) => {
-  const uid = "NhESkFqWjXaolAVkzyfKlgGZZFV2";
-  const res = (await req.json()) as PostType;
-  const userRecordRef = db.collection("users").doc(uid).collection("records");
-  const postRecordRef = await userRecordRef.add({
-    ...res,
-    createdAt: FieldValue.serverTimestamp(),
-    updatedAt: FieldValue.serverTimestamp(),
-  });
-  const postRecordSnap = await postRecordRef.get();
-  return NextResponse.json({
-    id: postRecordSnap.id,
-    ...postRecordSnap.data(),
-  });
+export const POST = async (request: NextRequest) => {
+  const idToken = request.headers.get("authorization");
+  if (!idToken)
+    return NextResponse.json(
+      { error: "認証情報がありません" },
+      { status: 401 }
+    );
+  try {
+    const decodedToken = await auth.verifyIdToken(idToken);
+    const uid = decodedToken.uid;
+    const userRecordRef = db.collection("users").doc(uid).collection("records");
+    const res = (await request.json()) as PostType;
+    const postRecordRef = await userRecordRef.add({
+      ...res,
+      createdAt: FieldValue.serverTimestamp(),
+      updatedAt: FieldValue.serverTimestamp(),
+    });
+    const postRecordSnap = await postRecordRef.get();
+    return NextResponse.json({
+      id: postRecordSnap.id,
+      ...postRecordSnap.data(),
+    });
+  } catch (error) {
+    return NextResponse.json(error);
+  }
 };
