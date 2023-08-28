fec6f98f45867249bd4f00a8a72a10bd99d274ca
yuto <yuto.0313king@icloud.com>
2023-08-28 12:10:15 +0900

    🔐 デプロイ用に環境変数をセット

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -2,7 +2,12 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = require("next-study-d2f7f-firebase-adminsdk-ph0ss-33e4d251cc.json");
+const encodedServiceAccountKey = Buffer.from(
+  process.env.FB_SERVICE_ACCOUNT_KEY!,
+  "base64"
+).toString();
+const serviceAccount = JSON.parse(encodedServiceAccountKey);
+// const serviceAccount = require("next-study-d2f7f-firebase-adminsdk-ph0ss-33e4d251cc.json");
 export const firebaseAdmin =
   getApps()[0] ??
   initializeApp({
