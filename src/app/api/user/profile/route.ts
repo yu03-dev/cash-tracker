925e9a098afd4749fd0c3ccf43a68e8adda3272c
yuto <yuto.0313king@icloud.com>
2023-09-09 09:12:09 +0900

    🐛 タイポ修正

--- src/app/api/user/profile/route.ts
+++ src/app/api/user/profile/route.ts
@@ -1,11 +1,13 @@
 import { db } from "@/firebase/admin";
-import { getUserInfo } from "@/utils/session";
+import { getUserInformation } from "@/utils/session";
 import { NextResponse, type NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   const sessionCookie = request.headers.get("Authorization");
   try {
-    const { uid, name, email, picture } = await getUserInfo(sessionCookie!);
+    const { uid, name, email, picture } = await getUserInformation(
+      sessionCookie!
+    );
     const userDocRef = db.collection("users").doc(uid);
     const userDocSnap = await userDocRef.get();
     return NextResponse.json({
