e402a2382346e757edeb2bac2f6cb2c6094f8694
yuto <yuto.0313king@icloud.com>
2023-09-15 04:22:16 +0900

    🩹 classNameの不要なプロパティを削除

--- src/app/(pages)/(contents)/records/add/page.tsx
+++ src/app/(pages)/(contents)/records/add/page.tsx
@@ -2,7 +2,7 @@ import { PostForm } from "@/app/(pages)/(contents)/records/add/components";
 
 export default function Page() {
   return (
-    <div className="w-full h-full flex flex-col gap-y-6 items-center px-1 py-8 sm:px-4 md:px-8 md:py-12 lg:px-12">
+    <div className="w-full flex flex-col gap-y-6 items-center px-1 py-8 sm:px-4 md:px-8 md:py-12 lg:px-12">
       <PostForm />
     </div>
   );
