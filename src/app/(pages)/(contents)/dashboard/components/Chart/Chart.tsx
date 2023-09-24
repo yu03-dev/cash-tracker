1ba61bb999f4c18a7bad00f4adbcff33a39872a5
yuto <yuto.0313king@icloud.com>
2023-09-24 18:48:24 +0900

    ♻️ Chartコンポーネントのリファクタリング

---
+++ src/app/(pages)/(contents)/dashboard/components/Chart/Chart.tsx
@@ -0,0 +1,80 @@
+"use client";
+import cn from "@/app/common/lib/cn";
+import { Card, Typography } from "@/app/common/lib/material-tailwind";
+import { ExpenseByCategoryType } from "@/types";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "@/app/common/lib/chart.js";
+import { Doughnut } from "@/app/common/lib/react-chartjs-2";
+import { ExpenseCategoryList } from "@/constant";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const backgroundColor = [
+  "rgba(255, 99, 132, 0.2)",
+  "rgba(54, 162, 235, 0.2)",
+  "rgba(255, 206, 86, 0.2)",
+  "rgba(75, 192, 192, 0.2)",
+  "rgba(153, 102, 255, 0.2)",
+  "rgba(255, 159, 64, 0.2)",
+];
+const borderColor = [
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)",
+];
+
+const ChartContent = ({ chartData }: { chartData: ExpenseByCategoryType }) => {
+  const expenses = ExpenseCategoryList.map((item) => {
+    return chartData.find((dataItem) => dataItem.category === item)?.value;
+  });
+
+  const data = {
+    labels: ExpenseCategoryList,
+    datasets: [
+      {
+        label: "金額(¥)",
+        data: expenses,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <Doughnut data={data} className="mx-auto" />;
+};
+
+type ChartProps = {
+  chartData: ExpenseByCategoryType;
+  className: string | undefined;
+};
+
+export const Chart = async (props: ChartProps) => {
+  const { chartData, className } = props;
+
+  // 要修正
+  if (chartData.length === 0) {
+    return (
+      <Typography
+        variant="h3"
+        color="blue-gray"
+        className="flex justify-center mt-4"
+      >
+        記録がありません。
+      </Typography>
+    );
+  }
+
+  return (
+    <Card className={cn(className)}>
+      <ChartContent chartData={chartData} />
+    </Card>
+  );
+};
--- src/app/(pages)/(contents)/dashboard/page.tsx
+++ src/app/(pages)/(contents)/dashboard/page.tsx
@@ -1,13 +1,19 @@
-import { MyChart } from "./components/Chart";
+import { fetchData } from "@/utils/fetchData";
+import { Chart } from "./components/Chart";
 import { ExpensesCard } from "./components/ExpensesCard";
 import { RecordList } from "./components/RecordList";
+import { ExpenseByCategoryType, zExpenseByCategory } from "@/types";
 
 export default async function Page() {
+  const chartData = await fetchData<ExpenseByCategoryType>({
+    uri: "/api/user/expenses/by-category",
+    schema: zExpenseByCategory,
+  });
   return (
     <div className="w-full h-full flex flex-col gap-y-6 items-center px-1 py-4 sm:px-4 md:px-8 lg:px-12">
       <div className="w-full flex flex-col gap-4 md:flex-row md:justify-center">
         <ExpensesCard className="w-full md:w-96 p-6" />
-        <MyChart className="w-full md:w-96 p-6" />
+        <Chart chartData={chartData} className="w-full md:w-96 p-6" />
       </div>
       <RecordList className="w-full overflow-y-scroll" />
     </div>
